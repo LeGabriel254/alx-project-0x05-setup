985448bd2157d88ff8515f0b121fe339211842e0
Leon Gabriel <g73346920@gmail.com>
2025-01-14 22:31:09 +0300

    Chored:Modified.

--- alx-project-0x05/context/CountContext.tsx
+++ alx-project-0x05/context/CountContext.tsx
@@ -1,22 +1,22 @@
 import { error } from "console";
-import { useState,ReactNode, createContext,useContext } from "react";
+import { useState, ReactNode, createContext, useContext } from "react";
 
-interface CounterContextProps {
-  count:number
-  increment: () => void;
-  decrement: () => void,
-};
+interface CountContextProps {
+  count: number
+  increment: () => void
+  decrement: () => void
+}
 
-export const CountContext = createContext<CounterContextProps | undefined>  (undefined)
+export const CountContext = createContext<CountContextProps | undefined>(undefined)
 
-export const CountProvider =({children}: {children:ReactNode}) =>{
+export const CountProvider = ({ children }: { children: ReactNode }) => {
   const [count, setCount] = useState(0)
 
-  const increment = () => setCount((count) => count + 1 )
-  const decrement = () => setCount((count) => count > 0 ? count - 1: 0 )
-  
+  const increment = () => setCount((count) => count + 1)
+  const decrement = () => setCount((count) => count > 0 ? count - 1 : 0)
+
   return (
-    <CountContext.Provider value ={{ count, increment,decrement}}>
+    <CountContext.Provider value={{ count, increment, decrement }}>
       {children}
     </CountContext.Provider>
   )
